2b0bf8d6f1d0e265758c754e457955bf404aa7e0
LMaria <liina.klaving@gmail.com>
2016-03-19 14:14:39 +0200

    kodutöö 5, ül. 2

---
+++ kodutoo5/abakus.js
@@ -0,0 +1,27 @@
+﻿window.onload = function () {
+
+    var parl = document.getElementsByClassName("bead");
+    for (var i = 0; i < parl.length; i++) {
+        parl[i].onclick = vahetaPoolt;
+
+
+    }
+
+
+    function vahetaPoolt() {
+        if (this.style.cssFloat == "left") {
+            this.style.cssFloat = "right";
+        } else {
+            this.style.cssFloat = "left";
+        }
+    }
+    
+    
+}
+			
+
+
+
+
+
+// lehe laadimine lõpetatud. Siia funktsiooni sisse kirjutan elementide mõjutamise käsud
