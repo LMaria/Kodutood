624d610d7968512a211667a44b0be5bcb555cb12
LMaria <liina.klaving@gmail.com>
2016-04-03 11:54:32 +0300

    tagurpidi sõna parandused

---
+++ kodutoo7/tagurpidi.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$tagurpidi = reverseString("tagurpidi sona");
+
+function reverseString($sona) {
+        $uus = ' ';
+	    for ($i = strlen($sona); $i >= 0; $i--) {
+		$temp = substr( $sona, $i, 1 );
+        $uus .= $temp;
+    }
+    return $uus;
+ }
+
+echo $tagurpidi;
+
+?>  
